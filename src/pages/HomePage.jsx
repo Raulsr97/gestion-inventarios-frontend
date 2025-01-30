7ef8d348a4f0ef4f7672434d068662dce7de99e7
Raulsr97 <salinasraul883@gmail.com>
2025-01-30 16:23:46 -0600

    configurando entorno

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">Bienvenido al Sistema de Inventario</h1>
+        </div>
+    )
+}
+
+export default HomePage
